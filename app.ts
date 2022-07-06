e8d8773c99488742014bbedac703702c01e899c3
Andrew Wong <andrew.j.wong@outlook.com>
2022-07-07 03:04:14 +1000

    Don't use web flow

--- app.ts
+++ app.ts
@@ -4,3 +4,47 @@ globalThis.logger = bunyan.createLogger({
     name: "Xero Test"
 })
 
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { XeroClient } from 'xero-node'
+
+const xero = new XeroClient({
+    clientId: process.env.XERO_CLIENT_ID,
+    clientSecret: process.env.XERO_CLIENT_SECRET,
+    redirectUris: ["https://this_is_a_test/redir"],
+    scopes: [
+        "openid",
+        "profile",
+        "email",
+        "accounting.transactions",
+        "offline_access"
+    ]
+})
+
+
+/*
+xero.buildConsentUrl().then(url => {
+    logger.info(url)
+})
+*/
+
+/*
+const url = "..."
+
+xero.apiCallback(url).then(token => {
+    console.log(token);
+})
+*/
+
+const token = {
+    id_token: '...',
+    /* 30 min expiry */
+    access_token: '...',
+    expires_at: 1657128627,
+    token_type: 'Bearer',
+    /* 60 day expiry */
+    refresh_token: '...',
+    scope: 'openid profile email accounting.transactions offline_access',
+    session_state: '...'
+}
