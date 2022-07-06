04732ae1a696e6f70ef170185084b1332f8595cf
Andrew Wong <andrew.j.wong@outlook.com>
2022-07-07 03:55:58 +1000

    Refactor

--- XeroWrapper.ts
+++ XeroWrapper.ts
@@ -1,40 +1,49 @@
 import { XeroClient } from "xero-node";
 import type { IXeroClientConfig } from "xero-node";
 
-let client: XeroClient;
-let loggerFunction: (msg, level?: number) => void
+type LoggerFunctionType = (msg, level?: number) => void
+class XeroWrapper {
+    #client: XeroClient;
+    #loggerFunction: LoggerFunctionType
 
-export function init({ clientId, clientSecret }: {
-    clientId: IXeroClientConfig['clientId'],
-    clientSecret: IXeroClientConfig['clientSecret'],
-}
-) {
-    client = new XeroClient({
-        clientId,
-        clientSecret,
-        grantType: 'client_credentials'
-    })
-}
+    constructor({ clientId, clientSecret, loggerFunction }: {
+        clientId: IXeroClientConfig['clientId'],
+        clientSecret: IXeroClientConfig['clientSecret'],
+        loggerFunction?: LoggerFunctionType
+    }
+    ) {
+        this.#client = new XeroClient({
+            clientId,
+            clientSecret,
+            grantType: 'client_credentials'
+        })
 
-export function setLogger(logFn: typeof loggerFunction) {
-    loggerFunction = logFn
-}
-function log(...args: Parameters<typeof loggerFunction>) {
-    loggerFunction && loggerFunction(...args)
-}
+        if (loggerFunction) this.#loggerFunction = loggerFunction
 
-/* TODO: Check if refresh in progress */
 
-export async function withXero(fn: (client: XeroClient) => Promise<any>): ReturnType<typeof fn> {
-    if (!client) throw new Error("Xero wrapper not initialised")
-
-    let auth = client.readTokenSet()
-    if (!auth || auth.expired()) {
-        if (!auth) log("Requesting Xero token")
-        else log("Xero token expired, refreshing")
-        await client.getClientCredentialsToken()
-        log("Got new token")
     }
 
-    return fn(client)
+    setLogger(loggerFunction: LoggerFunctionType) {
+        this.#loggerFunction = loggerFunction
+    }
+
+    #log(...args: Parameters<LoggerFunctionType>) {
+        this.#loggerFunction && this.#loggerFunction(...args)
+    }
+
+
+    async withXero(fn: (client: XeroClient) => Promise<any>): ReturnType<typeof fn> {
+
+        let auth = this.#client.readTokenSet()
+        if (!auth || auth.expired()) {
+            if (!auth) this.#log("Requesting Xero token")
+            else this.#log("Xero token expired, refreshing")
+            
+            /* TODO: Check if refresh in progress */
+            await this.#client.getClientCredentialsToken()
+            this.#log("Got new token")
+        }
+
+        return fn(this.#client)
+    }
 }
